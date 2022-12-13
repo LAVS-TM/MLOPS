06bddb25df22d70d064f84806a9d3d7596b69fa0
Victor Simonin <victor.simonin@epita.fr>
2022-12-13 17:03:05 +0100

    feat(mlflow): Add basic mlflow training

--- src/train_model.py
+++ src/train_model.py
@@ -1,13 +1,56 @@
-import pandas as pd 
+import warnings
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import joblib
+import mlflow
+import mlflow.sklearn
 
-def build_model():
-    df = pd.read_csv('../data/houses.csv')
-    X = df[['size', 'nb_rooms', 'garden']]
-    y = df['price']
-    model = LinearRegression()
-    model.fit(X, y)
-    joblib.dump(model, "model.joblib")
-    
-build_model()
+import logging
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(42)
+
+    data = pd.read_csv("../data/houses.csv")
+
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
+
+    # The predicted column is "price" 
+    train_x = train.drop(["price", "orientation"], axis=1)
+    test_x = test.drop(["price", "orientation"], axis=1)
+    train_y = train[["price"]]
+    test_y = test[["price"]]
+
+    with mlflow.start_run():
+        model = LinearRegression()
+        print(train_x)
+        model.fit(train_x, train_y)
+
+        predicted_qualities = model.predict(test_x)
+
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        mlflow.sklearn.log_model(model, "model")
+        joblib.dump(model, "model.joblib")
