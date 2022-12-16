46c6ca97b8ae0cd36bbc3122c1512fdcddd9bf60
alexandre.lemonnier <alexandre.lemonnier@epita.fr>
2022-12-16 13:45:10 +0100

    feat(final): fix dockerization, model prediction, and data generation

--- src/model_app.py
+++ src/model_app.py
@@ -1,4 +1,5 @@
 import request
+import os
 import streamlit as st
 import joblib
 import pandas as pd
@@ -22,6 +23,8 @@ st.dataframe(df)
 
 st.markdown("---")
 
+
+
 st.markdown("### Predict the price of a house:")
 
 size = st.number_input("Size (in m2)", min_value=0, max_value=1000, value=100)
@@ -29,21 +32,6 @@ nb_rooms = st.number_input(
     "Number of rooms", min_value=0, max_value=10, value=2)
 garden = st.checkbox("Garden")
 orientation = st.selectbox("Orientation", ["North", "South", "East", "West"])
-
-drift_df = pd.read_csv("datadrift_auc_train.csv")["auc"]
-# check last row of drift_df
-if drift_df.iloc[-1] > 0.5:
-    st.warning("Data drift detected")
-
-    if st.button("Reset model"):
-        try:
-            df = pd.read_csv("../data/houses.csv")
-            df["orientation"] = df["orientation"].map(
-                {"Nord": 0, "Est": 1, "Sud": 2, "Ouest": 3})
-            train_model(df)
-            st.write("Model retrained")
-        except:
-            st.write("Error during model training")
 
 
 if st.button("Predict"):
@@ -54,8 +42,9 @@ if st.button("Predict"):
         st.write("Error: could not connect to the API")
 
 st.markdown("---")
+
 nb_samples = st.number_input(
-    "Number of samples", min_value=0, max_value=10000, value=1000)
+    "Number of samples", min_value=10, max_value=10000, value=10)
 
 if st.button("Retrain model"):
     try:
@@ -67,10 +56,32 @@ if st.button("Retrain model"):
 
 st.markdown("---")
 
-
 st.markdown("### Data drift")
-drift_df = pd.read_csv("datadrift_auc_train.csv")["auc"]
-st.area_chart(drift_df)
+st.write("Here is the evolution of the AUC score of the model:")
+
+if os.path.exists("datadrift_auc_train.csv"):
+    drift_df = pd.read_csv("datadrift_auc_train.csv")["auc"]
+    # check last row of drift_df
+    if len(drift_df) != 0 and drift_df.iloc[-1] > 0.5:
+        st.warning("Data drift detected")
+
+        if st.button("Reset model"):
+            try:
+                os.remove("model.joblib")
+                os.remove("../data/new_houses.csv")
+                df = pd.read_csv("../data/houses.csv")
+                df["orientation"] = df["orientation"].map(
+                    {"Nord": 0, "Est": 1, "Sud": 2, "Ouest": 3})
+                train_model(df)
+                st.write("Model reset")
+            except:
+                st.write("Error during model reset")
+                
+if os.path.exists("datadrift_auc_train.csv"):
+    drift_df = pd.read_csv("datadrift_auc_train.csv")[["auc"]]
+    st.area_chart(drift_df)
+else:
+    st.area_chart(pd.DataFrame({"auc": [0]}))
 
 st.markdown("---")
 
@@ -82,7 +93,7 @@ with col1:
 
 with col2:
     st.image("../data/logo.jpg", width=400)
-    st.markdown("Project created by Alexandre Lemonnier and Victor Simnonin")
+    st.markdown("Project created by Alexandre Lemonnier and Victor Simonin")
 
 
 with col3:
