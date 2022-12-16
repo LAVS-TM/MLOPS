fad55a7fafddddd95318277704704e24ebf54984
alexandre.lemonnier <alexandre.lemonnier@epita.fr>
2022-12-16 09:37:21 +0100

    feat(dockerization): add docker compose to deploy project

--- src/model_app.py
+++ src/model_app.py
@@ -1,13 +1,11 @@
+import request
 import streamlit as st
 import joblib
 
-import request
-
-model = joblib.load("model.joblib")
-
 st.title("House price prediction")
 size = st.number_input("Size (in m2)", min_value=0, max_value=1000, value=100)
-nb_rooms = st.number_input("Number of rooms", min_value=0, max_value=10, value=2)
+nb_rooms = st.number_input(
+    "Number of rooms", min_value=0, max_value=10, value=2)
 garden = st.checkbox("Garden")
 orientation = st.selectbox("Orientation", ["North", "South", "East", "West"])
 
@@ -18,7 +16,8 @@ if st.button("Predict"):
     except:
         st.write("Error: could not connect to the API")
 
-nb_samples = st.number_input("Number of samples", min_value=0, max_value=10000, value=1000)
+nb_samples = st.number_input(
+    "Number of samples", min_value=0, max_value=10000, value=1000)
 if st.button("Retrain model"):
     try:
         request.retrain_request(nb_samples)
@@ -26,7 +25,7 @@ if st.button("Retrain model"):
     except:
         st.write("Error: could not connect to the API")
 
-col1, col2, col3 = st.columns([2,6,1])
+col1, col2, col3 = st.columns([2, 6, 1])
 
 with col1:
     st.write("")
